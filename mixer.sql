341561129dec8ee04a7b27a4206c4d20b8a123fc
lemonstener <deyanvasilev7@gmail.com>
2022-01-03 13:23:56 -0600

    changing things up, this will now be a mixology app

--- mixer.sql
+++ mixer.sql
@@ -2,19 +2,16 @@ DROP DATABASE IF EXISTS mixer_db;
 CREATE DATABASE mixer_db;
 \c mixer_db;
 
-CREATE TABLE cocktails (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    img VARCHAR(100) NOT NULL,
-    ingredients VARCHAR(100) NOT NULL,
-    measurments VARCHAR(100) NOT NULL,
-    instructions TEXT NOT NULL,
-    likes INT DEFAULT(0)
+    username VARCHAR(25) NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
 );
 
 CREATE TABLE ingredients (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL UNIQUE,
     description TEXT,
     type VARCHAR(20) NOT NULL,
     img_sm VARCHAR(100) NOT NULL,
@@ -23,11 +20,15 @@ CREATE TABLE ingredients (
     likes INT DEFAULT(0)
 );
 
-CREATE TABLE users (
+CREATE TABLE cocktails (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(25) NOT NULL,
-    email TEXT NOT NULL,
-    password TEXT NOT NULL
+    name VARCHAR(50) NOT NULL UNIQUE,
+    img VARCHAR(100) NOT NULL,
+    ingredients VARCHAR(100) NOT NULL,
+    measurments VARCHAR(100) NOT NULL,
+    instructions TEXT NOT NULL,
+    created_by INT NOT NULL REFERENCES users,
+    likes INT DEFAULT(0)
 );
 
 CREATE TABLE fav_cocktails (
