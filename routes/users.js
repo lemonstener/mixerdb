79e45e49279bf1b236cae8229b4d6894c9c6132f
lemonstener <deyanvasilev7@gmail.com>
2022-01-16 12:03:54 -0600

    more changes

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,7 @@ const req = require("express/lib/request");
 router.post("/register", async (req, res, next) => {
   try {
     const validator = jsonschema.validate(req.body, userRegisterSchema);
+    console.log(req.body);
     if (!validator.valid) {
       const errors = validator.errors.map((e) => e.stack);
       throw new ExpressError(errors, 400);
@@ -47,20 +48,6 @@ router.get("/:name", async (req, res, next) => {
   try {
     const user = await User.getByUsername(req.params.name);
     return res.status(200).json(user);
-    // if (user.rows.length > 0) {
-    //   return res.status(200).json(user.rows[0]);
-    // } else {
-    //   throw new ExpressError("User Not Found", 404);
-    // }
-  } catch (error) {
-    return next(error);
-  }
-});
-
-router.get("/favorites/:id", async (req, res, next) => {
-  try {
-    const result = await User.getFavorites(req.params.id);
-    return res.status(200).json(result);
   } catch (error) {
     return next(error);
   }
