958074b79de3dc09efadc7caa0db6b2f0d2a4690
lemonstener <deyanvasilev7@gmail.com>
2022-01-17 19:42:36 -0600

    asdasd

--- db.js
+++ db.js
@@ -1,20 +1,12 @@
 const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
-let client;
-
-if (process.env.NODE_ENV === "production") {
-  client = new Client({
-    connectionString: DB_URI,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-} else {
-  client = new Client({
-    connectionString: DB_URI,
-  });
-}
+const client = new Client({
+  connectionString: DB_URI,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 client.connect();
 
